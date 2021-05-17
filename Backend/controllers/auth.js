2efa999087c8ebb3f639a4d9581f33388cfc2e27
NicoJob13 <nico.jobard@yahoo.com>
2021-05-17 23:29:19 +0200

    Correction des RegExp -> Création user et connexion fonctionnelles - Commentaires.

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,63 +1,75 @@
-const bcrypt = require('bcrypt');
-const cryptojs = require ('crypto-js');
-const jwt = require('jsonwebtoken');
+/*-----------------------------------Les controllers destinés à l'authentification d'un utilisateur-------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------*/
 
-const User = require('../models/user');
+/***********************************************Appel des packages et schema nécessaires***********************************************/
+
+const bcrypt = require('bcrypt'); //Package pour chiffrement du mot de passe
+const cryptojs = require ('crypto-js'); //Package pour encodage de l'adresse e-mail
+const jwt = require('jsonwebtoken'); //Package pour création de token d'identification
+const User = require('../models/user'); //Schéma de création d'un utilisateur 
+
+/***********************************************Controller de création de l'utilisateur***********************************************/
 
 exports.signupUser = (req, res, next) => {
-    const passwordSchema = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[.-@$!%*?&])[0-9a-zA-Z.-@$!%*?&]{12,15}$/;
-    const emailSchema = /^[A-z0-9._-]+[@]{1}[a-zA-Z0-9._-]+[.]{1}[A-z]{2,}$/;
-    const userPassword = req.body.password;
+    //Récupération des information saisies dans les champs de formulaire
     const userEmail = req.body.email;
+    const userPassword = req.body.password;
     
-    if(userPassword.match(passwordSchema) && userEmail.match(emailSchema)) {
-        const encodedUserEmail = cryptojs.enc.Base64.stringify(cryptojs.enc.Utf8.parse(userEmail));
-        
-        bcrypt.hash(userPassword, 10)
-            .then(hash => {
-                const user = new User({
+    //Expressions régulières à vérifier pour l'e-mail et le mot de passe afin d'éviter les attaques
+    const emailSchema = /^[A-z0-9._-]+[@][a-zA-Z0-9._-]+[.][A-z]{2,}$/;
+    const passwordSchema = /^(?=.*?[0-9])(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[_!@#$£µ=§%^&*?-\\/\\]).{8,}$/;
+    
+    //Condition de création de l'utilisateur
+    if(userPassword.match(passwordSchema) && userEmail.match(emailSchema)) {//Si les 2 expressions sont vérifiées
+        const encodedUserEmail = cryptojs.enc.Base64.stringify(cryptojs.enc.Utf8.parse(userEmail)); //Encodage de l'e-mail
+        bcrypt.hash(userPassword, 10) //Hash du mot de passe par bcrypt avec 10 passes
+            .then(hash => {//En cas de réussite
+                const user = new User({//Création d'un nouvel utilisateur
                     email: encodedUserEmail,
                     password: hash
                 });
-                user.save()
-                    .then(() => {
-                        res.status(201).json({ message: 'Nouvel utilisateur enregistré' });
+                user.save() //Sauvegarde de l'utilisateur dans la base
+                    .then(() => {//En cas de réussite
+                        res.status(201).json({ message: 'Nouvel utilisateur enregistré' }); //Message de réussite dans la console
                         next();
                     })
-                    .catch(() => res.status(400).json({ error }));
+                    .catch(() => res.status(400).json({ error })); //En cas d'erreur on retourne l'erreur
             })
-            .catch(error => res.status(500).json({ error }));
-    } else {
-        throw new Error("L'adresse ou le mot de passe ne correspond pas à un format valide.")
+            .catch(error => res.status(500).json({ error })); //En cas d'erreur on retourne l'erreur
+    } else {//Si au moins l'une des expressions régulières n'est pas vérifiée
+        throw new Error("L'adresse ou le mot de passe ne correspond pas à un format valide."); //On retourne le message dans la console
     }
 };
 
+/***********************************************Controller de connexion de l'utilisateur**********************************************/
+
 exports.loginUser = (req, res, next) => {
+    //Récupération des information saisies dans les champs de formulaire
     const userPassword = req.body.password;
     const userEmail = req.body.email;
-    const encodedUserEmail = cryptojs.enc.Base64.stringify(cryptojs.enc.Utf8.parse(userEmail));
-    
+    const encodedUserEmail = cryptojs.enc.Base64.stringify(cryptojs.enc.Utf8.parse(userEmail)); //Encodage l'adresse e-mail saisie
+    //Recherche de l'utilisateur dans la base par correspondance des adresses saisie/enregistrée dans la base
     User.findOne({ email: encodedUserEmail })
         .then(user => {
-            if(!user) {
+            if(!user) {//Si aucune correspondance n'est trouvée
                 return res.status(401).json({ error: 'Utilisateur non trouvé' });
             }
-            bcrypt.compare(userPassword, user.password)
+            bcrypt.compare(userPassword, user.password) //Sinon on compare les mots de passe saisi/enregistré dans la base
                 .then(valid => {
-                    if(!valid) {
+                    if(!valid) {//S'ils ne correspondent pas
                         return res.status(401).json({ error: 'Mot de passe incorrect' });
                     }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
+                    res.status(200).json({ //S'ils correspondent la réponse contient :
+                        userId: user._id, //le userId,
+                        token: jwt.sign( //un token d'authentification encodé
+                            { userId: user._id }, //contenant lui-même le userId,
+                            'RANDOM_TOKEN_SECRET', //la clé d'encodage,
+                            { expiresIn: '24h' } //la durée de validité du token
                         )
                     });
                     next();
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => res.status(500).json({ error })); //En cas d'erreur on retourne l'erreur
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ error })); //En cas d'erreur on retourne l'erreur
 };
