7ab583b5f4d0a9afb3efdcf531c4aa8a3420f4e1
NicoJob13 <nico.jobard@yahoo.com>
2021-05-18 10:33:48 +0200

    Commentaire des fichiers de gestion de l'authentification (modèle, routeur, middleware).

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,12 +1,22 @@
-const mongoose = require('mongoose');
+/*--------------------------------------------------Schéma applicable à l'objet 'user'--------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------*/
+//Ce schéma permet de créer un modèle précis applicable à chaque création (inscription) d'un utilisateur
 
-const uniqueValidator = require('mongoose-unique-validator');
+/****************************************************Appel des packages nécessaires****************************************************/
 
-const userSchema = mongoose.Schema({
+const mongoose = require('mongoose'); //Package mongoose
+const uniqueValidator = require('mongoose-unique-validator'); //Package destiné à s'assurer de l'unicité du compte
+
+/**********************************************************Création du schéma**********************************************************/
+//Utilisation de la méthode 'Schema' de mongoose
+
+const userSchema = mongoose.Schema({ //On spécifie les champs souhaités dans l'objet avec leurs attributs
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator); //Application du plugin assurant l'unicité, au schéma
+
+/***********************Export du modèle avec un nom, 'User', pour le rendre utilisable dans les autres fichiers***********************/
 
 module.exports = mongoose.model('User', userSchema);
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,11 +1,21 @@
-const express = require('express');
+/*-------------------------------------------Les routes destinées à gérer l'authentification--------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------*/
+//Ces routes vont permettre à l'utilisateur de créer son compte et de se connecter à l'application.
+
+/*********************************************Appel des package et controllers nécessaires*********************************************/
+
+const express = require('express'); //Package express pour créer le routeur
+const authCtrl = require('../controllers/auth'); //Controllers applicables à l'authentification, contenant la logique de fonctionnement
+
+/**********************************************************Création du routeur*********************************************************/
 
 const router = express.Router();
 
-const authCtrl = require('../controllers/auth');
+/************************************Les routes permettant d'appliquer la logique de fonctionnement************************************/
 
-router.post('/signup', authCtrl.signupUser);
+router.post('/signup', authCtrl.signupUser); //Inscription d'un utilisateur
+router.post('/login', authCtrl.loginUser); //Connexion de l'utilisateur
 
-router.post('/login', authCtrl.loginUser);
+/****************************Export du router pour utilisation dans le fichier principal de l'application*****************************/
 
 module.exports = router;
